d04709e034bc6a42bc5c810461bf48d694b60b07
Russell Williams <rw_flyer@hotmail.com>
2012-11-13 18:06:49 -0800

    Add recursive processing

--- process_images.sh
+++ process_images.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-mogrify -resize 1920x1024 *.jpg
-jhead -autorot *.jpg
+# Scan for and fix bogus file extensions
+find -iname "*.jpeg" -print
+
+# Scan for and delete bogus filename turds left by MacOS
+find -name "._*.jpg" -print
+
+# Recursive resize
+find -iname "*.jpg" -exec mogrify -resize 1920x1024 {} \;
+
+# Recursive iOS image rotation fixups
+find -iname "*.jpg" -exec jhead -autorot {} \;
+
+#mogrify -resize 1920x1024 *.jpg
+#jhead -autorot *.jpg
